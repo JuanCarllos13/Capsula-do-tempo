da350f8aa19b69290cb54b308a928c9ec15b3983
Juan Carlos <juan2017amaral@gmail.com>
2023-05-17 13:46:31 -0400

    class 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,14 @@
+import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
+import { memoriesRoutes } from "./routes/memory";
 
 const app = fastify();
+
+app.register(memoriesRoutes);
+
+app.register(fastifyCors, {
+  origin: true
+});
 
 app
   .listen({
