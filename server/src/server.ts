561c1f6025c62ab4d7fa55ab897e5ff8e7de7514
Juan Carlos <juan2017amaral@gmail.com>
2023-05-19 13:12:36 -0400

    class 3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,27 @@
 import fastifyCors from "@fastify/cors";
+import fastifyJwt from "@fastify/jwt";
+import "dotenv/config";
 import fastify from "fastify";
+import { authRoutes } from "./routes/auth";
 import { memoriesRoutes } from "./routes/memory";
 
 const app = fastify();
 
+app.register(authRoutes);
 app.register(memoriesRoutes);
 
+app.register(fastifyJwt, {
+  secret: 'spacetime'
+})
+
 app.register(fastifyCors, {
-  origin: true
+  origin: true,
 });
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0'
   })
   .then(() => {
     console.log("Servido Online🚀");
